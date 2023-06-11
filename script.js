8e6f0ac90ca28bd804ab4f496e01c62081cca40e
= <44709674+ZackGabri@users.noreply.github.com>
2023-06-11 13:50:02 +0300

    Changed number of parallel requests from 3 to 2

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getRatingsPublicAPI() {
 	let loadedCount = 1;
 	let ratings = [];
 
-	const chunkSize = 3;
+	const chunkSize = 2;
 	for (let i = 0; i < playerList.length; i += chunkSize) {
 		const chunk = playerList.slice(i, i + chunkSize);
 
